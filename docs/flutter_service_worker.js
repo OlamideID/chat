e45515a2381a8b25801f3d03a232851fc6094c17
OlamideID <iakinola926@gmail.com>
2024-12-02 09:20:42 +0100

    new look and features

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -34,14 +34,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "8a4533f554eea73b953bc1c0338d0c00
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "8d129a0939c5d6afaa5bb5d78fe3f15b",
+"flutter_bootstrap.js": "fc47e55b15fda4b33c972ea2ec8f3fcd",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "73893e03d013818b81bc6ad83410b7ad",
 "/": "73893e03d013818b81bc6ad83410b7ad",
-"main.dart.js": "d73899be01d8b14ae6e311b8ef6d83fd",
+"main.dart.js": "d06270f91dfe3dcbd69dc0e8ef599bf9",
 "manifest.json": "8fb212bd05708f02648ab2e3bebce9e7",
 "version.json": "8e7bd9742e1f54b1ca5c443ab0e74c96"};
 // The application shell files that are downloaded before a service worker can
