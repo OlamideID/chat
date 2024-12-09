08e3d88e80eb68123e03dd0cc05421910f2367c5
OlamideID <iakinola926@gmail.com>
2024-12-09 15:09:58 +0100

    new

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
-import 'package:flutter/foundation.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -34,12 +33,31 @@ class MyApp extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final theme = ref.watch(themeProvider);
-
     return DevicePreview(
       backgroundColor: Colors.white,
-      enabled: !kReleaseMode, // Only enable device preview in debug mode
+      enabled: true,
       availableLocales: const [
         Locale('en', 'US'),
+      ],
+      tools: const [
+        DeviceSection(
+          model: true,
+          orientation: false,
+          frameVisibility: false,
+          virtualKeyboard: false,
+        )
+      ],
+      // Set a custom list of devices to display in Device Preview
+      devices: [
+        Devices.macOS.macBookPro,
+        Devices.linux.laptop,
+        Devices.android.samsungGalaxyA50,
+        Devices.android.samsungGalaxyS20,
+        Devices.ios.iPhone13ProMax,
+        Devices.ios.iPhoneSE,
+        Devices.ios.iPhone13Mini,
+        Devices.ios.iPad,
+        Devices.windows.laptop,
       ],
       builder: (context) => MaterialApp(
         // ignore: deprecated_member_use
