0754abaf9d112f70867d1e8333414a95ac010076
OlamideID <iakinola926@gmail.com>
2024-12-10 08:58:17 +0100

    new

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "408252989"
+    serviceWorkerVersion: "654574609"
   }
 });
